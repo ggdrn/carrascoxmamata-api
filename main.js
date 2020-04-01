49f35bc1b1369cdd2a87c733014d39153924a2e3
Diego Lamarão <diego.lamarao92@gmail.com>
2020-04-01 08:56:09 -0300

    add comment

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ import api from "./src/api";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-exports.server = functions.https.onRequest(api);
+// Exporting function api
+exports.api = functions.https.onRequest(api);
+
