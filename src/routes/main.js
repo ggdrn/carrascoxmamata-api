a09bf88160419249b0d7ffa26fcb49730bc93adc
Diego Lamarão <diego.lamarao92@gmail.com>
2020-04-03 09:51:07 -0300

    add router middleware sample

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,10 @@
 import { sendApiStatus } from "Controllers/main";
 
 export default (router) => {
+  // ROUTER MIDDLEWARES
+  // router.use(myCustomMiddleware)
+
+  // ROUTER ROUTES
   router.get("/", sendApiStatus);
   return router;
 };
